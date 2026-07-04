b540c9ca888d69391b2bdf4977653c53784f6f14
aymanebenomar <aymanebenomar2005@gmail.com>
2026-07-04 09:45:10 +0100

    chore: update default target URL and increase load test user stages

--- test-load.js
+++ test-load.js
@@ -11,15 +11,15 @@ import { check, sleep } from 'k6';
 // k6 run -e WEBSITE_URL=https://your-website.com tests/simple-load-test.js
 // ============================================================
 
-const WEBSITE_URL = __ENV.WEBSITE_URL || 'https://test.k6.io';
+const WEBSITE_URL = __ENV.WEBSITE_URL || 'https://carreera.com';
 
 export const options = {
   stages: [
     // The load goes up every 3 minutes
-    { duration: '3m', target: 1 },   // 0-3 min: 1 user
-    { duration: '3m', target: 5 },   // 3-6 min: 5 users
-    { duration: '3m', target: 10 },  // 6-9 min: 10 users
-    { duration: '3m', target: 15 },  // 9-12 min: 15 users
+    { duration: '3m', target: 50 },   // 0-3 min: 1 user
+    { duration: '3m', target: 90 },   // 3-6 min: 5 users
+    { duration: '3m', target: 180 },  // 6-9 min: 10 users
+    { duration: '3m', target: 260 },  // 9-12 min: 15 users
     { duration: '3m', target: 0 },   // 12-15 min: stop the test
   ],
 
@@ -39,7 +39,6 @@ export default function () {
     'status is OK': (r) => r.status >= 200 && r.status < 400,
     'response time is under 1000ms': (r) => r.timings.duration < 1000,
   });
-
   // Wait 1 second before the same virtual user sends another request
   sleep(1);
 }
